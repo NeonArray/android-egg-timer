4d46d60fc720f15bbeefd29b6b569aec5add6c3c
Aaron Arney <aaron.g.arney@gmail.com>
2017-12-21 11:45:33 -0500

    Add resetTimer method

--- app/src/main/java/com/ocularrhythm/eggtimer/MainActivity.java
+++ app/src/main/java/com/ocularrhythm/eggtimer/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -15,6 +16,21 @@ public class MainActivity extends AppCompatActivity {
 
     SeekBar timerSeekBar;
     TextView timerTextView;
+    Button controllerButton;
+    Boolean timerIsActive = false;
+    CountDownTimer countDownTimer;
+
+    public void resetTimer() {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        v.vibrate(1000);
+
+        timerTextView.setText("0:30");
+        timerSeekBar.setProgress(30);
+        countDownTimer.cancel();
+        controllerButton.setText("Start");
+        timerSeekBar.setEnabled(true);
+        timerIsActive = false;
+    }
 
     public void updateTimer(int secondsLeft) {
         int minutes = secondsLeft / 60;
@@ -27,19 +43,26 @@ public class MainActivity extends AppCompatActivity {
 
     public void controlTimer(View view) {
 
-        new CountDownTimer(timerSeekBar.getProgress() *  1000 + 100, 1000) {
+        if (!timerIsActive) {
+            timerIsActive = true;
+            timerSeekBar.setEnabled(false);
+            controllerButton.setText("Stop");
 
-            @Override
-            public void onTick(long millisUntilFinished) {
-                updateTimer((int) millisUntilFinished / 1000);
-            }
+            countDownTimer = new CountDownTimer(timerSeekBar.getProgress() *  1000 + 100, 1000) {
 
-            @Override
-            public void onFinish() {
-                Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-                v.vibrate(1000);
-            }
-        }.start();
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    updateTimer((int) millisUntilFinished / 1000);
+                }
+
+                @Override
+                public void onFinish() {
+                    resetTimer();
+                }
+            }.start();
+        } else {
+            resetTimer();
+        }
     }
 
     @Override
@@ -49,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
 
         timerSeekBar = findViewById(R.id.timerSeekBar);
         timerTextView = findViewById(R.id.timerTextView);
+        controllerButton = findViewById(R.id.controllerButton);
 
         timerSeekBar.setMax(600);
         timerSeekBar.setProgress(30);
